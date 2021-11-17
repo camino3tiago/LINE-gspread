84e5317de2ba91d698fa95438152b47e61e082a1
camino3tiago <h.mocky1703@gmail.com>
2021-11-17 18:36:53 +0900

    modified .env-file

--- line-gspread.py
+++ line-gspread.py
@@ -70,8 +70,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
-CHANNEL_ACCESS_TOKEN = os.getenv('LINE-GSPREAD_CHANNEL_ACCESS_TOKEN')
-CHANNEL_SECRET = os.getenv('LINE-GSPREAD_CHANNEL_SECRET')
+CHANNEL_ACCESS_TOKEN = os.getenv('LINE_GSPREAD_CHANNEL_ACCESS_TOKEN')
+CHANNEL_SECRET = os.getenv('LINE_GSPREAD_CHANNEL_SECRET')
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
